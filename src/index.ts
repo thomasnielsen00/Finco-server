ee33052ee192b1d01d75b7401c05b1a9e6ebe1e6
thomasnielsen00 <115644894+thomasnielsen00@users.noreply.github.com>
2023-03-22 19:42:50 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from "express";
 import companyRouter from "./company-router";
 import userRouter from "./user-router";
+const cors = require("cors");
+
+
 // import path from 'path';
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(cors());
+
 
 // Serve client files
 // app.use(express.static(path.join(__dirname, '/../../client/public')));

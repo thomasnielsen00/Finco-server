8e596ac17e9724a82455571bedc175daf124d390
thomasnielsen00 <you@example.com>
2023-03-22 17:02:56 +0100

    ENV

--- dist/mysql-pool.js
+++ dist/mysql-pool.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql2_1 = __importDefault(require("mysql2"));
 const pool = mysql2_1.default.createPool({
-    host: "35.228.18.247",
-    user: "ADMIN",
-    password: 'l@XuS-G~$a$Mp)"C',
-    database: "finco-dev",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     // socketPath: process.env.CLOUD_SQL_CONNECTION_NAME,
     // Reduce load on Google Cloud MySQL server
     connectionLimit: 10,
--- src/mysql-pool.ts
+++ src/mysql-pool.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
 
 const pool = mysql.createPool({
-  host: "35.228.18.247",
-  user: "ADMIN",
-  password: 'l@XuS-G~$a$Mp)"C',
-  database: "finco-dev",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   // socketPath: process.env.CLOUD_SQL_CONNECTION_NAME,
   // Reduce load on Google Cloud MySQL server
   connectionLimit: 10,

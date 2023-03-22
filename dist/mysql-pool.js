5d4409c6dc4c04a2fe397e0308272adf04657ca5
thomasnielsen00 <you@example.com>
2023-03-22 18:17:12 +0100

    deploytest

--- dist/mysql-pool.js
+++ dist/mysql-pool.js
@@ -4,14 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql2_1 = __importDefault(require("mysql2"));
+require("dotenv").config();
 const pool = mysql2_1.default.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    // socketPath: process.env.CLOUD_SQL_CONNECTION_NAME,
+    //   socketPath: process.env.CLOUD_SQL_CONNECTION_NAME,
     // Reduce load on Google Cloud MySQL server
-    connectionLimit: 10,
+    connectionLimit: 5,
     // Convert MySQL boolean values to JavaScript boolean values
     typeCast: (field, next) => field.type == "TINY" && field.length == 1 ? field.string() == "1" : next(),
 });
--- src/mysql-pool.ts
+++ src/mysql-pool.ts
@@ -1,13 +1,14 @@
 import mysql from "mysql2";
+require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  // socketPath: process.env.CLOUD_SQL_CONNECTION_NAME,
+  //   socketPath: process.env.CLOUD_SQL_CONNECTION_NAME,
   // Reduce load on Google Cloud MySQL server
-  connectionLimit: 10,
+  connectionLimit: 5,
   // Convert MySQL boolean values to JavaScript boolean values
   typeCast: (field, next) =>
     field.type == "TINY" && field.length == 1 ? field.string() == "1" : next(),
